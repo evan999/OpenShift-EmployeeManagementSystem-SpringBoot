c7ea61a133791e7f03a9df36b4de508a06fc641d
evan999 <emeshberge@gmail.com>
2021-11-12 23:25:06 -0500

    Build new deployment file

--- src/main/java/com/openshift/employeemanagement/repositories/EmployeeRepository.java
+++ src/main/java/com/openshift/employeemanagement/repositories/EmployeeRepository.java
@@ -3,10 +3,12 @@ package com.openshift.employeemanagement.repositories;
 import com.openshift.employeemanagement.entities.Employee;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Date;
 import java.util.List;
 
+@Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
     List<Employee> findAllByRole(String role, Sort sort);
     List<Employee> findAllByDepartment(String department, Sort sort);
